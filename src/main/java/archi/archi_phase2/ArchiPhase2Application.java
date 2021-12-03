d5e1e99e5981f5870c5c5585fc70cd6239803bc1
Thanh Duy Dang <duyunikdang@gmail.com>
2021-12-03 16:27:37 +0100

    First Commit

---
+++ src/main/java/archi/archi_phase2/ArchiPhase2Application.java
@@ -0,0 +1,14 @@
+package archi.archi_phase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiPhase2Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hellworld");
+		SpringApplication.run(ArchiPhase2Application.class, args);
+	}
+
+}
---
+++ src/test/java/archi/archi_phase2/ArchiPhase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package archi.archi_phase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiPhase2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
